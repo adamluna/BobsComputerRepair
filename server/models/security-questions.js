bf5017a9fc689b16d8a9113dbbb58993b81fbe7e
Adam Luna <adam.luna@outlook.com>
2021-09-15 21:30:50 -0500

    restart security questions file

--- server/models/security-questions.js
+++ server/models/security-questions.js
@@ -8,17 +8,9 @@
 */
 
 // require statements
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+
 
 // create securityquestions schema
-let securityQuestionSchema = new Schema(
-    {
-        questionId: {type: String},
-        answer: {type: String},
-    },
-    { collection: "securityQuestions"} // specify collection in database
-);
+
 
 // export module
-module.exports = mongoose.model("SecurityQuestions", securityQuestionSchema);
