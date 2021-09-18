48e5f85432863879a6ec9ad3597164117adebf18
Adam Luna <adam.luna@outlook.com>
2021-09-18 01:29:12 -0500

    update mongodb connection string

--- server/app.js
+++ server/app.js
@@ -39,7 +39,7 @@ app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
 const port = process.env.PORT || 3000; // server port
 
 // TODO: This line will need to be replaced with your actual database connection string
-const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/bcrs?retryWrites=true&w=majority';
+const conn = 'mongodb+srv://bcrs_user:admin@buwebdev-cluster-1.j3npe.mongodb.net/bcrs?retryWrites=true&w=majority';
 
 /**
  * Database connection
