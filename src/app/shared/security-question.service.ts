c3ea61e61e0f667f58c8e9c298322670551492f3
Adam Luna <adam.luna@outlook.com>
2021-09-19 13:48:29 -0500

    Fix syntax errors

--- src/app/shared/security-question.service.ts
+++ src/app/shared/security-question.service.ts
@@ -14,6 +14,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SecurityQuestionService {
+  static findSecurityQuestionById: any;
+  static updatedSecurityQuestion: any;
+  static findAllSecurityQuestions: any;
+  static deleteSecurityQuestion: any;
 
   constructor(private http: HttpClient) { }
 
